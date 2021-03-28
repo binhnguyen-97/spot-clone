108f9f07eab2b278fcb6ffe17a782cd62033a099
binhnguyen <binhnguyen@chotot.vn>
2021-03-29 00:25:05 +0700

    add config file

---
+++ src/config.js
@@ -0,0 +1,10 @@
+const config = {
+  api: {
+    baseUrl: 'https://api.spotify.com/v1',
+    authUrl: 'https://accounts.spotify.com/api/token',
+    clientId: '',
+    clientSecret: ''
+  }
+}
+
+export default config
